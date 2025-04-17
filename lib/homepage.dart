9b1a290820053b6e72bf810411ee2af574f3ed5d
nayra92 <naira.hashem@gmail.com>
2025-04-17 20:05:43 +0300

    modified:   lib/homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,6 +11,36 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            myContainer(Colors.yellow, Icons.home),
+            myContainer(Colors.amber, Icons.favorite),
+            myContainer(Colors.black, Icons.access_alarm),
+            myContainer(Colors.blue, Icons.person_3),
+          ],
+        ),
+      )
+    );
   }
+}
+
+Widget myContainer (
+  Color mycolor,
+  IconData myicondata
+){
+  return Padding(
+    padding: const EdgeInsets.all(5.0),
+    child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: mycolor,
+            borderRadius: BorderRadius.circular(15)
+          ),
+          child: Icon(myicondata, color: Colors.white,),
+        ),
+  );
 }
